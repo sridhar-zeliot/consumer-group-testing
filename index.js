0f7d52391a9aa6909e4aae20de88ae063cdff60f
sridhar <sridhar@zeliot.in>
2025-11-27 12:50:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Kafka } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: process.env.CLIENT_ID,
+  brokers: ['my-cluster-kafka-bootstrap.kafka.svc:9092'],
+  sasl: {
+    mechanism: "scram-sha-512",
+    username: process.env.SASL_USERNAME,
+    password: process.env.SASL_PASSWORD,
+  },
+  namespace: "kafka"
+});
+
+const consumer = kafka.consumer({
+  groupId: process.env.CONSUMER_GROUP_ID,
+});
+
+const run = async () => {
+  await consumer.connect();
+
+  // Subscribe to both topics
+  await consumer.subscribe({
+    topics: ["test-schema-car-protobuf", "test-schema-car-avro"],
+    fromBeginning: true,
+  });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log(`📥 Consumed from ${topic} [${partition}]`);
+      console.log("Key:", message.key?.toString());
+      console.log("Value (raw):", message.value.toString());
+      console.log("Headers:", message.headers);
+      console.log("--------------------------------------");
+    },
+  });
+};
+
+run().catch(console.error);
